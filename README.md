c14afc1791653eac895744809a588c6f4cad3df8
Kamil S Jaroň <376090@mail.muni.cz>
2017-03-22 17:30:55 +0100

    stats scripts separated

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 #### Functions
 
 [make_data_frame.R](make_data_frame.R) : function that created an empty data.frame from a vector of column names.
+[variables.R](variables.R) : contains commonly used variables in project
---
+++ variables.R
@@ -0,0 +1,8 @@
+# file with comon variables
+
+              #ASEX    #SEX
+timemas <- c("1_Tdi", "1_Tps",
+             "2_Tsi", "2_Tcm",
+             "3_Tms", "3_Tce",
+             "4_Tte", "4_Tbi",
+             "5_Tge", "5_Tpa")
