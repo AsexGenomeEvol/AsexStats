5db8f872abff2a7f1038b94a7b1f978d1eda7589
KamilSJaron <kamil.jaron@unil.ch>
2017-04-18 14:28:28 +0000

    Merge branch 'master' of github.com:AsexGenomeEvol/timema_assembly

--- get_suboptimal_asm.R
+++ get_suboptimal_asm.R
@@ -3,8 +3,8 @@ sp = commandArgs(trailingOnly=TRUE)[1]
 scf_file <- paste0('stats/assemblies/',sp,'_scfs.tsv')
 ctg_file <- paste0('stats/assemblies/',sp,'_ctgs.tsv')
 
-scf_asm <- read.table(scf_file)
-ctg_asm <- read.table(ctg_file)
+scf_asm <- read.table(scf_file, header = T)
+ctg_asm <- read.table(ctg_file, header = T)
 
 all_dir <- unique(c(as.character(scf_asm$dir), as.character(ctg_asm$dir)))
 
@@ -16,5 +16,7 @@ present <- dir(paste0('data/',sp,'/assembly/'))
 
 suboptimal_present <- suboptimal[suboptimal %in% present]
 
-write(paste("Keeping:",paste(opt, collapse = " ")), stderr())
+write(sp, stderr())
+write(paste("Keeping:", opt[1], scf_asm[scf_asm$dir == opt[1],'NG50']), stderr())
+write(paste("Keeping:", opt[2], ctg_asm[ctg_asm$dir == opt[2],'NG50']), stderr())
 cat(paste(suboptimal_present, collapse = " "))
