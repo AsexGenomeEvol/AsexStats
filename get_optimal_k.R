66a80e00883bcaa667bee1adb2c65342d3e76e8f
Kamil S Jaroň <376090@mail.muni.cz>
2017-04-19 15:19:56 +0200

    README have dependencies; make have batch 3

---
+++ get_optimal_k.R
@@ -0,0 +1,8 @@
+sp <- commandArgs(trailingOnly=TRUE)[1]
+
+source(paste0(Sys.getenv('TROOT'), '/scripts/R/variables.R'))
+
+# should probvably read kmergenie output, nevermind
+opt_k <- c(87,83,87,83,89,83,81,81,87,65)[sp == timemas]
+
+cat(opt_k,"\n")
