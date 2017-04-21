0213a43bc1e00a603cd24ed3b6a92c09dd3b92d9
KamilSJaron <kamil.jaron@unil.ch>
2017-04-21 17:06:46 +0000

    reordered stats by adjusted quality score

--- get_optimal_asm.R
+++ get_optimal_asm.R
@@ -5,11 +5,17 @@
 #asm <- read.table('stats/assemblies/1_Tdi_scfs.tsv', header= T)
 
 get_asm_scores <- function(asm){
-  NG50_scores <- asm$NG50 / max(asm$NG50)
-  total_diff_scores <- 1 - abs(asm$diff_in_sum) / 1300000000
+  N50_scores <- asm$N50 / max(asm$N50, na.rm=TRUE)
+  NG50_scores <- asm$NG50 / max(asm$NG50, na.rm=TRUE)
+  neg_size_score <- asm$diff_in_sum
+  neg_size_score[neg_size_score > 0] <- 0
+  neg_size_score <- 1 - abs(neg_size_score) / 430000000 # if the assembly is half of size as expected, penalise by 1
+  pos_size_score <- asm$diff_in_sum
+  pos_size_score[pos_size_score < 0] <- 0
+  pos_size_score <- 1 - abs(pos_size_score) / 1300000000 # if the assembly is twice size as expected, penalise by 1
 
 
-  asm$score <- rowMeans(cbind(NG50_scores, total_diff_scores), na.rm=TRUE)
+  asm$score <- rowMeans(cbind(N50_scores, NG50_scores, neg_size_score, pos_size_score), na.rm=TRUE)
   return(asm)
 }
 
