db23456dc5e3d86469b0f7552c44624b13d9a6eb
KamilSJaron <kamil.jaron@unil.ch>
2017-04-23 19:13:52 +0000

    make indexed contigs, map reads on contigs

--- get_optimal_k.R
+++ get_optimal_k.R
@@ -5,4 +5,4 @@ source(paste0(Sys.getenv('TROOT'), '/scripts/R/variables.R'))
 # should probvably read kmergenie output, nevermind
 opt_k <- c(87,83,87,83,89,83,81,81,87,65)[sp == timemas]
 
-cat(opt_k,"\n")
+cat(opt_k)#,"\n")
