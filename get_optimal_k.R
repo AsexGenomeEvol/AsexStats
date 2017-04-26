92d3444fa4c99c83834a89c39017e6f7a07dfaff
Kamil S Jaroň <376090@mail.muni.cz>
2017-04-26 17:18:25 +0200

    gapfilling automated (not tested)

--- get_optimal_k.R
+++ get_optimal_k.R
@@ -1,8 +1,21 @@
-sp <- commandArgs(trailingOnly=TRUE)[1]
+args <- commandArgs(trailingOnly=TRUE)
+sp <- args[1]
+if(length(args) == 1){
+  batch = 'kmergenie'
+} else {
+  batch = args[2]
+}
 
-source(paste0(Sys.getenv('TROOT'), '/scripts/R/variables.R'))
+troot <- Sys.getenv('TROOT')
+source(paste0(troot, '/scripts/R/variables.R'))
+source(paste0(troot, '/scripts/R/batch_subset.R'))
 
-# should probvably read kmergenie output, nevermind
-opt_k <- c(87,83,87,83,89,83,81,81,87,65)[sp == timemas]
+if(batch == 'kmergenie' | batch == 3){
+  # should probvably read kmergenie output, nevermind
+  opt_k <- c(87,83,87,83,89,83,81,81,87,65)[sp == timemas]
+} else {
+  asm <- read.table(paste0(troot, '/stats/assemblies/',sp,'_scfs.tsv'), header = T)
+  opt_k <- batch_subset(asm, batch)$kmer[1]
+}
 
 cat(opt_k)#,"\n")
