ef752af3d64daf9ee273a47d5bcd4d9056fde08e
Kamil S Jaroň <376090@mail.muni.cz>
2017-06-01 18:03:05 +0200

    timema names for printing tables

--- variables.R
+++ variables.R
@@ -16,6 +16,12 @@ pair_labels <- c(expression(italic("T. douglasi / T. poppensis")),
                  expression(italic("T. tahoe / T. barmani")),
                  expression(italic("T. genevieve / T. podura")))
 
+timema_names <- c("T. douglasi","T. poppensis",
+                  "T. shepardi","T. californicum",
+                   "T. monikensis","T. cristinae",
+                   "T. tahoe","T. barmani",
+                   "T. genevieve","T. podura")
+
 timema_labels <- c(expression(italic("T. douglasi")) , expression(italic("T. poppensis")),
                    expression(italic("T. shepardi")) , expression(italic("T. californicum")),
                    expression(italic("T. monikensis")) , expression(italic("T. cristinae")),
