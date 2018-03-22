cf2b911a8605ef0088ba6a93bb01c5fd95a8da70
Kamil S Jaroň <376090@mail.muni.cz>
2018-03-22 13:54:47 +0100

    switched order of asexuals by median divergence to sexuals

--- data-raw/timema_pairs.R
+++ data-raw/timema_pairs.R
@@ -1,14 +1,15 @@
 timema_pair_labels <- c(expression(italic("T. tahoe / T. bartmani")),
-                 expression(italic("T. monikensis / T. cristinae")),
-                 expression(italic("T. douglasi / T. poppensis")),
-                 expression(italic("T. shepardi / T. californicum")),
-                 expression(italic("T. genevieve / T. podura")))
+                        expression(italic("T. shepardi / T. californicum")),
+                        expression(italic("T. douglasi / T. poppensis")),
+                        expression(italic("T. monikensis / T. cristinae")),
+                        expression(italic("T. genevieve / T. podura")))
 
-timema_JC_divergence <- c(0.77, 1.09, 1.17, 1.32, 1.88)
-
+timema_JC_divergence_mean <- c(0.77, 1.32, 1.17, 1.09, 1.88)
+timema_JC_divergence_median <- c(0.24, 0.36, 0.38, 0.46, 0.72)
 
 timema_pairs <- list(labels = timema_pair_labels,
-                      JC_divergence = timema_JC_divergence)
+                      JC_median_divergence = timema_JC_divergence_median,
+                      JC_mean_divergence = timema_JC_divergence_mean)
 
 
 save(timema_pairs, file = "data/timema_pairs.rdata")
--- data-raw/timemas.R
+++ data-raw/timemas.R
@@ -1,38 +1,38 @@
                    #ASEX    #SEX
 timema_codes <- c("4_Tte", "4_Tbi",
-                  "3_Tms", "3_Tce",
-                  "1_Tdi", "1_Tps",
                   "2_Tsi", "2_Tcm",
+                  "1_Tdi", "1_Tps",
+                  "3_Tms", "3_Tce",
                   "5_Tge", "5_Tpa")
 
 timema_names <- c("T. tahoe", "T. bartmani",
-                  "T. monikensis", "T. cristinae",
-                  "T. douglasi", "T. poppensis",
                   "T. shepardi", "T. californicum",
+                  "T. douglasi", "T. poppensis",
+                  "T. monikensis", "T. cristinae",
                   "T. genevieve", "T. podura")
 
 timema_labels <- c(expression(italic("T. tahoe")) , expression(italic("T. bartmani")),
-                   expression(italic("T. monikensis")) , expression(italic("T. cristinae")),
-                   expression(italic("T. douglasi")) , expression(italic("T. poppensis")),
                    expression(italic("T. shepardi")) , expression(italic("T. californicum")),
+                   expression(italic("T. douglasi")) , expression(italic("T. poppensis")),
+                   expression(italic("T. monikensis")) , expression(italic("T. cristinae")),
                    expression(italic("T. genevieve")) , expression(italic("T. podura")))
 
 timema_short_labels <- c(expression(italic("Tte")) , expression(italic("Tbi")),
-                         expression(italic("Tms")) , expression(italic("Tce")),
-                         expression(italic("Tdi")) , expression(italic("Tps")),
                          expression(italic("Tsi")) , expression(italic("Tcm")),
+                         expression(italic("Tdi")) , expression(italic("Tps")),
+                         expression(italic("Tms")) , expression(italic("Tce")),
                          expression(italic("Tge")) , expression(italic("Tpa")))
 
 timema_latex_labels <- c("\\textcolor{asex_blue}{\\textit{tahoe}}","\\textcolor{sex_red}{\\textit{bartmani}}",
-                         "\\textcolor{asex_blue}{\\textit{monikensis}}","\\textcolor{sex_red}{\\textit{cristinae}}",
-                         "\\textcolor{asex_blue}{\\textit{douglasi}}","\\textcolor{sex_red}{\\textit{poppensis}}",
                          "\\textcolor{asex_blue}{\\textit{shepardi}}","\\textcolor{sex_red}{\\textit{californicum}}",
+                         "\\textcolor{asex_blue}{\\textit{douglasi}}","\\textcolor{sex_red}{\\textit{poppensis}}",
+                         "\\textcolor{asex_blue}{\\textit{monikensis}}","\\textcolor{sex_red}{\\textit{cristinae}}",
                          "\\textcolor{asex_blue}{\\textit{genevievae}}", "\\textcolor{sex_red}{\\textit{podura}}")
 
 timema_optimal_kmer <- c(81, 81,
+                         87, 83,
+                         87, 83,
                          89, 83,
-                         87, 83,
-                         87, 83,
                          87, 65)
 
 cols <- c('#a6bddb', '#fc9272',
