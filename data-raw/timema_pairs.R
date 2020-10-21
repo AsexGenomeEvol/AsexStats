83910d03c92377333d6fad69a94feadd1574327c
KamilSJaron <kamiljaron@gmail.com>
2020-10-21 19:20:50 +0100

    switching order of 3 and 1

--- data-raw/timema_pairs.R
+++ data-raw/timema_pairs.R
@@ -1,15 +1,16 @@
 timema_pair_labels <- c(expression(italic("T. tahoe / T. bartmani")),
                         expression(italic("T. shepardi / T. californicum")),
-                        expression(italic("T. douglasi / T. poppensis")),
                         expression(italic("T. monikensis / T. cristinae")),
+                        expression(italic("T. douglasi / T. poppensis")),
                         expression(italic("T. genevievae / T. podura")))
 
-timema_JC_divergence_mean <- c(0.77, 1.32, 1.17, 1.09, 1.88)
-timema_JC_divergence_median <- c(0.24, 0.36, 0.38, 0.46, 0.72)
+timema_JC_divergence_mean <- c(0.77, 1.32, 1.09, 1.17, 1.88)
+timema_JC_divergence_median <- c(0.24, 0.36, 0.46, 0.38, 0.72)
+timema_divergences <- c(0.00166475, 0.00228019, 0.00275862, 0.00343811, 0.00559974)
 
 timema_pairs <- list(labels = timema_pair_labels,
                       JC_median_divergence = timema_JC_divergence_median,
-                      JC_mean_divergence = timema_JC_divergence_mean)
-
+                      JC_mean_divergence = timema_JC_divergence_mean,
+                      phylo_divergences = timema_divergences)
 
 save(timema_pairs, file = "data/timema_pairs.rdata")
--- data-raw/timemas.R
+++ data-raw/timemas.R
@@ -1,45 +1,45 @@
                    #ASEX    #SEX
 timema_codes <- c("4_Tte", "4_Tbi",
                   "2_Tsi", "2_Tcm",
-                  "1_Tdi", "1_Tps",
                   "3_Tms", "3_Tce",
+                  "1_Tdi", "1_Tps",
                   "5_Tge", "5_Tpa")
 
 timema_names <- c("T. tahoe", "T. bartmani",
                   "T. shepardi", "T. californicum",
-                  "T. douglasi", "T. poppensis",
                   "T. monikensis", "T. cristinae",
+                  "T. douglasi", "T. poppensis",
                   "T. genevievae", "T. podura")
 
 timema_labels <- c(expression(italic("T. tahoe")) , expression(italic("T. bartmani")),
                    expression(italic("T. shepardi")) , expression(italic("T. californicum")),
-                   expression(italic("T. douglasi")) , expression(italic("T. poppensis")),
                    expression(italic("T. monikensis")) , expression(italic("T. cristinae")),
+                   expression(italic("T. douglasi")) , expression(italic("T. poppensis")),
                    expression(italic("T. genevievae")) , expression(italic("T. podura")))
 
 timema_short_labels <- c(expression(italic("Tte")) , expression(italic("Tbi")),
                          expression(italic("Tsi")) , expression(italic("Tcm")),
-                         expression(italic("Tdi")) , expression(italic("Tps")),
                          expression(italic("Tms")) , expression(italic("Tce")),
+                         expression(italic("Tdi")) , expression(italic("Tps")),
                          expression(italic("Tge")) , expression(italic("Tpa")))
 
 timema_latex_labels <- c("\\textcolor{asex_blue}{\\textit{tahoe}}","\\textcolor{sex_red}{\\textit{bartmani}}",
                          "\\textcolor{asex_blue}{\\textit{shepardi}}","\\textcolor{sex_red}{\\textit{californicum}}",
-                         "\\textcolor{asex_blue}{\\textit{douglasi}}","\\textcolor{sex_red}{\\textit{poppensis}}",
                          "\\textcolor{asex_blue}{\\textit{monikensis}}","\\textcolor{sex_red}{\\textit{cristinae}}",
+                         "\\textcolor{asex_blue}{\\textit{douglasi}}","\\textcolor{sex_red}{\\textit{poppensis}}",
                          "\\textcolor{asex_blue}{\\textit{genevievae}}", "\\textcolor{sex_red}{\\textit{podura}}")
 
 timema_optimal_kmer <- c(81, 81,
                          87, 83,
-                         87, 83,
                          89, 83,
+                         87, 83,
                          87, 65)
 
 cols <- c('#a6bddb', '#fc9272',
           '#74a9cf', '#fb6a4a',
-		  '#3690c0', '#ef3b2c',
-		  '#0570b0', '#cb181d',
-		  '#045a8d', '#a50f15')
+          '#3690c0', '#ef3b2c',
+          '#0570b0', '#cb181d',
+          '#045a8d', '#a50f15')
 
 timemas <- list(codes = timema_codes,
                 names = timema_names,
